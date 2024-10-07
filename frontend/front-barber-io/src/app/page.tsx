369d76d18d4691a85d2b3a1b00a147a3eebd17de
gabryel-araujo <gabryel.dias@academico.ifpb.edu.br>
2024-10-07 00:26:31 -0300

    configurações iniciais do projeto no front-end

---
+++ frontend/front-barber-io/src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+import Login from "./components/Login";
+
+export default function Home() {
+  return (
+    <>
+      <h1>BarberIO</h1>
+      <p>Primeiro codigo usando TS</p>
+      <Login />
+    </>
+  );
+}
