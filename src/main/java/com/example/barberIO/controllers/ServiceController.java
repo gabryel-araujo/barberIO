cbd05875e1eaf27be5802ae05ff333db8f7ff57e
gabryel <gabryelaraujo71@gmail.com>
2025-04-17 22:03:18 -0300

    feat: criacao do service controller

---
+++ src/main/java/com/example/barberIO/controllers/ServiceController.java
@@ -0,0 +1,61 @@
+package com.example.barberIO.controllers;
+
+import com.example.barberIO.dtos.ServiceRecordDto;
+import com.example.barberIO.models.ServiceModel;
+import com.example.barberIO.repositories.ServiceRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+public class ServiceController {
+    @Autowired
+    ServiceRepository serviceRepository;
+
+    @GetMapping("/servico")
+    ResponseEntity<List<ServiceModel>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(serviceRepository.findAll());
+    }
+
+    @PostMapping("/servico")
+    ResponseEntity<Object> addServico(@RequestBody @Valid ServiceRecordDto serviceRecordDto){
+        ServiceModel serviceModel = new ServiceModel();
+        BeanUtils.copyProperties(serviceRecordDto,serviceModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(serviceRepository.save(serviceModel));
+    }
+
+    @PutMapping("/servico/{id}")
+    ResponseEntity<Object> editarServico(@PathVariable(value = "id") UUID id, @RequestBody @Valid ServiceRecordDto serviceRecordDto){
+        Optional<ServiceModel> serviceO = serviceRepository.findById(id);
+
+        if(serviceO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Serviço não encontrado!");
+        }
+        ServiceModel serviceModel = serviceO.get();
+        BeanUtils.copyProperties(serviceRecordDto,serviceModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body(serviceRepository.save(serviceModel));
+    }
+
+    @DeleteMapping("/servico/{id}")
+    ResponseEntity<Object> apagarServico(@PathVariable(value = "id")UUID id){
+        Optional<ServiceModel> serviceO = serviceRepository.findById(id);
+
+        if(serviceO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Serviço não encontrado!");
+        }
+
+        serviceRepository.delete(serviceO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Serviço removido!");
+    }
+
+}
