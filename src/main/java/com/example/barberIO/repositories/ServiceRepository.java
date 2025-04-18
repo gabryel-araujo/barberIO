8fc01d21c8b882eec036274662c4ad65828ca702
gabryel <gabryelaraujo71@gmail.com>
2025-04-17 22:03:44 -0300

    feat: criacao do service repository

---
+++ src/main/java/com/example/barberIO/repositories/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.example.barberIO.repositories;
+
+import com.example.barberIO.models.ServiceModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ServiceRepository extends JpaRepository<ServiceModel, UUID> {
+}
