693503fecdcbcd91750208776ce51e6863cc2e05
“renatowillon” <renatowillon@hotmail.com>
2025-04-23 14:00:16 -0300

    feat: inicio da tela de agendamento de cliente

--- front-barberio/src/app/(agendar)/agendar/page.tsx
+++ front-barberio/src/app/(agendar)/agendar/page.tsx
@@ -1,7 +1,72 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 const agendar = () => {
+  const [passo, setPasso] = useState(1);
+
+  function proximoPasso() {
+    setPasso(passo + 1);
+  }
+  function anteriorPasso() {
+    setPasso(passo - 1);
+  }
+
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <p>página de agendamento de horario com o barbeiro</p>
+    <div className="w-full h-full flex flex-col items-center justify-center space-y-6">
+      <p className="text-3xl font-bold">
+        Agende seu <span className="texto-azul">horário</span>
+      </p>
+      <div className="bg-slate-200 rounded-full flex gap-3 px-3 py-2">
+        <div
+          className={`rounded-full text-sm size-7 flex items-center justify-center
+            ${passo === 1 ? "bg-azul text-white" : ""}
+            ${passo > 1 ? "bg-azul-50 text-white" : ""}
+            ${passo < 1 ? "bg-white text-black" : ""}
+            `}
+        >
+          1
+        </div>
+        <div
+          className={`rounded-full text-sm size-7 flex items-center justify-center
+            ${passo === 2 ? "bg-azul text-white" : ""}
+            ${passo > 2 ? "bg-azul-50 text-white" : ""}
+            ${passo < 2 ? "bg-white text-black" : ""}
+            `}
+        >
+          2
+        </div>
+        <div
+          className={`rounded-full text-sm size-7 flex items-center justify-center
+            ${passo === 3 ? "bg-azul text-white" : ""}
+            ${passo > 3 ? "bg-azul-50 text-white" : ""}
+            ${passo < 3 ? "bg-white text-black" : ""}
+            `}
+        >
+          3
+        </div>
+        <div
+          className={`rounded-full text-sm size-7 flex items-center justify-center
+            ${passo === 4 ? "bg-azul text-white" : ""}
+            ${passo > 4 ? "bg-azul-50 text-white" : ""}
+            ${passo < 4 ? "bg-white text-black" : ""}
+            `}
+        >
+          4
+        </div>
+      </div>
+      <div className="flex gap-3">
+        <Button
+          variant="ghost"
+          className="cursor-pointer hover:bg-slate-200"
+          onClick={anteriorPasso}
+        >
+          Antes
+        </Button>
+        <Button className="cursor-pointer" onClick={proximoPasso}>
+          Proximo
+        </Button>
+      </div>
     </div>
   );
 };
