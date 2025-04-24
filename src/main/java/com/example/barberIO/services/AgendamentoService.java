19ae96bf6a3daed4095af9b48543b9a38b2b8b7f
gabryel <gabryelaraujo71@gmail.com>
2025-04-24 20:42:32 -0300

    feat: criacao da classe service que irá controlar as regras de negocio da aplicação

---
+++ src/main/java/com/example/barberIO/services/AgendamentoService.java
@@ -0,0 +1,78 @@
+package com.example.barberIO.services;
+
+import com.example.barberIO.controllers.AgendamentoController;
+import com.example.barberIO.controllers.ServiceController;
+import com.example.barberIO.dtos.AgendamentoRecordDto;
+import com.example.barberIO.models.AgendamentoModel;
+import com.example.barberIO.models.ClienteModel;
+import com.example.barberIO.models.FuncionarioModel;
+import com.example.barberIO.models.ServiceModel;
+import com.example.barberIO.repositories.AgendamentoRepository;
+import com.example.barberIO.repositories.ClienteRepository;
+import com.example.barberIO.repositories.FuncionarioRepository;
+import com.example.barberIO.repositories.ServiceRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+
+    public ResponseEntity<Object> agendarHorario(AgendamentoRecordDto agendamentoRecordDto){
+        Optional<FuncionarioModel> funcionarioO = funcionarioRepository.findById(agendamentoRecordDto.barbeiro().getId());
+        Optional<ClienteModel> clienteO = clienteRepository.findById(agendamentoRecordDto.cliente().getId());
+        Optional<ServiceModel> servicoO = serviceRepository.findById(agendamentoRecordDto.servico().getId());
+
+        if(funcionarioO.isEmpty() || clienteO.isEmpty() || servicoO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dados inválidos para a requisição. Cliente, Serviço ou Barbeiro inválidos");
+        }
+
+        FuncionarioModel barbeiro = funcionarioO.get();
+        ServiceModel servico = servicoO.get();
+        ClienteModel cliente = clienteO.get();
+
+        LocalDateTime horarioAgendamento = agendamentoRecordDto.horario();
+        LocalDateTime fimAgendamento = horarioAgendamento.plusMinutes(servico.getDuracao());
+
+        boolean barbeiroOcupado = agendamentoRepository.barbeiroOcupado(barbeiro.getId(),horarioAgendamento, fimAgendamento);
+
+        if(barbeiroOcupado){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("O barbeiro está ocupado no horário solicitado");
+        }
+        else {
+            AgendamentoModel agendamentoModel = new AgendamentoModel();
+            BeanUtils.copyProperties(agendamentoRecordDto, agendamentoModel);
+
+            agendamentoModel.setBarbeiro(barbeiro);
+            agendamentoModel.setCliente(cliente);
+            agendamentoModel.setServico(servico);
+            agendamentoModel.setHorario(horarioAgendamento);
+            agendamentoModel.setFim(fimAgendamento);
+            agendamentoModel.setFim(agendamentoModel.getFim());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(agendamentoRepository.save(agendamentoModel));
+        }
+
+    }
+
+
+}
