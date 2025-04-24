eccf9aa576bb9793ad215055808693c72575026c
gabryel <gabryelaraujo71@gmail.com>
2025-04-24 20:43:12 -0300

    fix: inserção da anotation @Repository

--- src/main/java/com/example/barberIO/repositories/ServiceRepository.java
+++ src/main/java/com/example/barberIO/repositories/ServiceRepository.java
@@ -2,8 +2,11 @@ package com.example.barberIO.repositories;
 
 import com.example.barberIO.models.ServiceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<ServiceModel, Long> {
 }
