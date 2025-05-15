df0ff1afe90e36078b74d8676fb86f88c5f675cd
gabryel <gabryelaraujo71@gmail.com>
2025-05-14 22:59:29 -0300

    feat: componente para form do usuário

---
+++ front-barberio/src/app/(agendar)/agendar/components/dadosCliente.tsx
@@ -0,0 +1,50 @@
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+
+export const schema = z.object({
+  name: z.string().min(3, "O nome deve ter no mínimo 3 caracteres"),
+  email: z.string().email("Email inválido"),
+  phone: z.string().min(11, "Telefone inválido"),
+});
+
+export type FormData = z.infer<typeof schema>;
+
+type DadosClienteProps = {
+  register: UseFormRegister<FormData>;
+  errors: FieldErrors<FormData>;
+};
+
+export function DadosCliente({ register, errors }: DadosClienteProps) {
+  return (
+    <div className="flex flex-col gap-3">
+      <form>
+        <div>
+          <p className="text-sm">Nome</p>
+          <Input type="text" {...register("name")} />
+          {errors.name && (
+            <p className="text-red-500 italic text-xs">{errors.name.message}</p>
+          )}
+        </div>
+        <div>
+          <p className="text-sm">Email</p>
+          <Input type="email" {...register("email")} />
+          {errors.email && (
+            <p className="text-red-500 italic text-xs">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <p className="text-sm">Telefone</p>
+          <Input type="text" {...register("phone")} />
+          {errors.phone && (
+            <p className="text-red-500 italic text-xs">
+              {errors.phone.message}
+            </p>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
