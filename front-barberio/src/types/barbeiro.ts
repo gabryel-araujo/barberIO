f914e2fe4c9e1b83832fe7f9f89b0b960b561f4c
gabryel <gabryelaraujo71@gmail.com>
2025-05-15 18:19:45 -0300

    feat: ciração provisória do BarbeiroAPI para conseguir fazer as primeiras requisições

--- front-barberio/src/types/barbeiro.ts
+++ front-barberio/src/types/barbeiro.ts
@@ -1,7 +1,22 @@
 export interface Barbeiro {
   id: string;
   nome: string;
+  email: string;
+  senha: string;
+  data_nascimento: string;
   avatar?: string;
   servicos: string[];
   isDisponivel: boolean;
+}
+
+export interface BarbeiroApi {
+  id: number;
+  nome: string;
+  email: string;
+  senha: string;
+  data_nascimento: string;
+  avaliacao: number;
+  experiencia: number;
+  atendimentos: number;
+  servicos: [];
 }
