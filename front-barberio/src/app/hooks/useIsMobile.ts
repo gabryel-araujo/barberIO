8506101a9ac2e0f8db0b037dbdbcf2e38b808ba5
“renatowillon” <renatowillon@hotmail.com>
2025-06-19 15:54:27 -0300

    fix: Ajuste Layout MobileSIdebar para ter scroll

--- front-barberio/src/app/hooks/useIsMobile.ts
+++ front-barberio/src/app/hooks/useIsMobile.ts
@@ -1,7 +1,7 @@
 import { useMediaQuery } from "react-responsive";
 
 const useIsMobile = () => {
-  return useMediaQuery({ query: `(max-width: 760px)` });
+  return useMediaQuery({ query: `(max-width: 999px)` });
 };
 
 export default useIsMobile;
