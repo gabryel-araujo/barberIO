ca22a0ab5f57a86c411a2060ce7f9f0cb4f5be08
“renatowillon” <renatowillon@hotmail.com>
2025-07-27 14:27:02 -0300

    feat: melhoria no calendario para não repetir codigo

--- front-barberio/src/app/(agendar)/agendar/components/step1.tsx
+++ front-barberio/src/app/(agendar)/agendar/components/step1.tsx
@@ -135,13 +135,15 @@ export const Step1 = () => {
             console.log(dataSelecionada);
           }}
           disabled={(date) => {
-            const hoje = new Date();
-            hoje.setHours(0, 0, 0, 0);
-            const isBeforeToday = date < hoje;
-            const isSunday = date.getDay() === 0;
-            const dataStr = date.toISOString().slice(0, 10);
-            const isFeriados = arrayFeriadoData.includes(dataStr);
-            return isBeforeToday || isSunday || isFeriados;
+            return isDataInvalida(date);
+
+            // const hoje = new Date();
+            // hoje.setHours(0, 0, 0, 0);
+            // const isBeforeToday = date < hoje;
+            // const isSunday = date.getDay() === 0;
+            // const dataStr = date.toISOString().slice(0, 10);
+            // const isFeriados = arrayFeriadoData.includes(dataStr);
+            // return isBeforeToday || isSunday || isFeriados;
           }}
           className="w-[250px] rounded-md border shadow"
         />
