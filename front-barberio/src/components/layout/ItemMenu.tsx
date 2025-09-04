eb437d738855129f46eb849276f4af00e5ce4e94
“renatowillon” <renatowillon@hotmail.com>
2025-09-04 14:02:15 -0300

    feat: BI-65 deixar no calendario disable quando o dia na configuração estiver fechado

--- front-barberio/src/components/layout/ItemMenu.tsx
+++ front-barberio/src/components/layout/ItemMenu.tsx
@@ -54,7 +54,7 @@ const menuItems: MenuItem[] = [
     permission: "GESTOR",
   },
   {
-    path: "/configuracao?empresaId=3",
+    path: "/configuracao?empresaId=1",
     icon: <Settings className="h-5 w-5" />,
     label: "Configuração",
     permission: "GESTOR",
