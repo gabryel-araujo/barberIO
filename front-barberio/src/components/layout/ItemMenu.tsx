d44110491698ae4dfc27a057f3caa8fe55f38864
gabryel-true <gabryelaraujo71@gmail.com>
2025-09-29 17:34:05 -0300

    FIX ROLES

--- front-barberio/src/components/layout/ItemMenu.tsx
+++ front-barberio/src/components/layout/ItemMenu.tsx
@@ -27,37 +27,37 @@ const menuItems: MenuItem[] = [
     path: "/dashboard",
     icon: <BarChart className="h-5 w-5" />,
     label: "Dashboard",
-    permission: "ROLE_GESTOR",
+    permission: "GESTOR",
   },
   {
     path: "/agendamentos",
     icon: <Calendar className="h-5 w-5" />,
     label: "Agendamentos",
-    permission: "ROLE_GESTOR",
+    permission: "GESTOR",
   },
   {
     path: "/clientes",
     icon: <User className="h-5 w-5" />,
     label: "Clientes",
-    permission: "ROLE_GESTOR",
+    permission: "GESTOR",
   },
   {
     path: "/barbeiros",
     icon: <Users className="h-5 w-5" />,
     label: "Barbeiros",
-    permission: "ROLE_GESTOR",
+    permission: "GESTOR",
   },
   {
     path: "/servicos",
     icon: <Scissors className="h-5 w-5" />,
     label: "Serviços",
-    permission: "ROLE_GESTOR",
+    permission: "GESTOR",
   },
   {
     path: "/configuracao?empresaId=1",
     icon: <Settings className="h-5 w-5" />,
     label: "Configuração",
-    permission: "ROLE_GESTOR",
+    permission: "GESTOR",
   },
 ];
 
